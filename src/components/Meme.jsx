35426a956946def5efb53ffb177003c52b6f3f54
Erica Layton <e.alexislayton@gmail.com>
2022-11-30 19:44:20 -0800

    first commit

---
+++ src/components/Meme.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import memesData from "../memesData";
+import { useState } from "react";
+
+const Meme = () => {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form--input" />
+        <input type="text" placeholder="Bottom text" className="form--input" />
+        <button onClick={getMemeImage} className="form--button">
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <img src={meme.randomImage} alt="meme image" className="meme--image" />
+    </main>
+  );
+};
+
+export default Meme;
